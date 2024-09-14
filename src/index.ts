79767e591a8a83601221a9fc4399b97771aaff7e
anix001 <chaudharyanish305@gmail.com>
2024-09-14 10:25:39 +0545

    Improvement: Folder str.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import express, {Express, Request, Response} from "express";
-import dotenv from "dotenv";
+import cors from "cors";
 
-//[Configure Dotenv]
-dotenv.config();
+import { config } from "./utils";
+import { corsOptions, errorHandler } from "./middleware";
+import appRoutes from "./routes"
 
 
 const app:Express = express();
-const port = process.env.PORT || 8000;
+const port = config.port;
 
 //[for json parse]
 app.use(express.json());
+//[for cors]
+app.use(cors(corsOptions));
+
+
+//routes
+app.use("/api", appRoutes);
 
 app.get('/', (req:Request, res:Response)=>{
     res.send("Express + Typescript Boilerplate");
 });
+
+//[error middleware]
+app.use(errorHandler);
 
 app.listen(port, ()=>{
     console.log(`[server]: Server is running on port ${port}!!`);
