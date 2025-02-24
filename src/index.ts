1d9eb034e3626782a74f17dc19cc970ce23ce6d8
anix001 <chaudharyanish305@gmail.com>
2025-02-24 12:39:20 +0545

    Improvement: Logger

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 
-import { config } from "./utils";
+import { config, Logger } from "./utils";
 import { corsOptions, errorHandler } from "./middleware";
-import appRoutes from "./routes"
+import appRoutes from "./routes";
 
-
-const app:Express = express();
+const app: Express = express();
 const port = config.port;
 
 //[for json parse]
 app.use(express.json());
 //[for cors]
 app.use(cors(corsOptions));
-
+//[for logging API Request]
+app.use((req: Request, res: Response, next: NextFunction) => {
+  Logger.info(`Incoming Request: ${req.method} ${req.url}`);
+  next();
+});
 
 //routes
 app.use("/api", appRoutes);
 
-app.get('/', (req:Request, res:Response)=>{
-    res.send("Express + Typescript Boilerplate");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + Typescript Boilerplate");
 });
 
 //[error middleware]
 app.use(errorHandler);
 
-app.listen(port, ()=>{
-    console.log(`[server]: Server is running on port ${port}!!`);
+app.listen(port, () => {
+  Logger.info(`Server is running on port ${port}!!`);
+  console.log(`[server]: Server is running on port ${port}!!`);
 });
-
--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,11 +1,13 @@
 import {Request, Response, NextFunction} from "express";
 import { HttpError } from "../interface";
+import { Logger } from "../utils";
 
 export const errorHandler = (err:HttpError, req:Request, res:Response, next:NextFunction):void=>{
     const status = err.status || 500;
     const message = err.message || 'Internal Server Error';
 
     //Log the error
+    Logger.error(`${status} - ${message}`);
     console.error(`Error: ${status} - ${message}`);
 
     //Send error response
